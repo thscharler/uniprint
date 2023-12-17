ce3a9fbc75bcaf66df96a158e04871f087867301
thscharler <thscharler@gmail.com>
2023-12-17 14:16:43 +0100

    feat: printer_attr for linux.
    non-functional, continue on a linux machine.

--- src/linux.rs
+++ src/linux.rs
@@ -1,3 +1,10 @@
+//!
+//!
+//! References:
+//! https://www.rfc-editor.org/rfc/rfc8011
+//! https://www.cups.org
+//! https://stackoverflow.com/questions/44687154/more-complete-list-of-cups-printer-state-reasons
+
 use std::ffi::{c_int, CStr, CString};
 use std::io;
 use std::io::{ErrorKind, Write};
@@ -6,8 +13,8 @@ use std::ptr::{self, slice_from_raw_parts};
 use cups_sys::ippGetInteger;
 use cups_sys::{
     cupsCheckDestSupported, cupsCopyDestInfo, cupsCreateJob, cupsFindDestReady, cupsFinishDocument,
-    cupsFreeDestInfo, cupsFreeDests, cupsGetDests, cupsLastErrorString, cupsStartDocument,
-    cupsWriteRequestData, cups_dest_t, cups_option_t,
+    cupsFreeDestInfo, cupsFreeDests, cupsGetDests, cupsGetNamedDest, cupsLastErrorString,
+    cupsStartDocument, cupsWriteRequestData, cups_dest_t, cups_option_t,
 };
 use cups_sys::{
     http_status_e_HTTP_STATUS_CONTINUE as HTTP_STATUS_CONTINUE, http_t,
@@ -54,32 +61,188 @@ pub fn default_printer() -> std::io::Result<String> {
     }
 }
 
+// printer-state
+//      "3" if the destination is idle,
+//      "4" if the destination is printing a job,
+//      "5" if the destination is stopped.
+//
+// printer-state-reasons from https://stackoverflow.com/questions/44687154/more-complete-list-of-cups-printer-state-reasons
+//
+// none
+// other
+// developer-low
+// door-open
+// fuser-over-temp
+// fuser-under-temp
+// input-tray-missing
+// interlock-open
+// interpreter-resource-unavailable
+// marker-supply-empty
+// marker-supply-low
+// marker-waste-almost-full
+// marker-waste-full
+// media-empty
+// media-jam
+// media-low
+// media-needed
+// moving-to-paused
+// opc-life-over
+// opc-near-eol
+// output-area-almost-full
+// output-area-full
+// output-tray-missing
+// paused
+// shutdown
+// spool-area-full
+// stopped-partly
+// stopping
+// timed-out
+// toner-empty
+// toner-low
+//
+// However the source code of job.c seems to also mention the following statuses (including the two mention in the question). I think this makes an exhaustive list until more come along.
+//
+// connecting-to-device
+// offline-report
+// cups-insecure-filter-warning
+// cups-missing-filter-warning
+// cups-remote-aborted
+// cups-remote-canceled
+// cups-remote-completed
+// cups-remote-pending
+// cups-remote-pending-held
+// cups-remote-processing
+// cups-remote-stopped
+// cups-waiting-for-job-completed
+
+#[non_exhaustive]
+#[derive(Default, Debug, Clone)]
+pub struct Info {
+    // -- common --
+    /// CUPS: name
+    /// WIN: pPrinterName
+    pub printer_name: String,
+    /// CUPS: printer-uri-supported
+    /// WIN: \\\\pserverName\\pPrinterName
+    pub printer_uri: String,
+    /// CUPS: device-uri
+    /// WIN: pPortName
+    pub device_uri: String,
+    /// CUPS: printer-make-and-model
+    /// WIN: pDriverName
+    pub driver_name: String,
+    /// CUPS: printer-info
+    /// WIN: comment
+    pub printer_info: String,
+    /// CUPS: printer-location
+    /// WIN: location
+    pub printer_location: String,
+    /// CUPS: job-priority
+    /// WIN: DefaultPriority
+    pub job_priority: u32,
+
+    // -- cups --
+    pub printer_instance: Option<String>,
+    pub job_cancel_after: u32,
+    pub job_hold_until: String,
+    pub job_sheets: String,
+    pub marker_change_time: u32,
+
+    pub status_none: bool,
+    pub status_other: bool,
+    pub status_developer_low: bool,
+    pub status_door_open: bool,
+    pub status_fuser_over_temp: bool,
+    pub status_fuser_under_temp: bool,
+    pub status_input_tray_missing: bool,
+    pub status_interlock_open: bool,
+    pub status_interpreter_resource_unavailable: bool,
+    pub status_marker_supply_empty: bool,
+    pub status_marker_supply_low: bool,
+    pub status_waste_almost_full: bool,
+    pub status_waste_full: bool,
+    pub status_media_empty: bool,
+    pub status_media_jam: bool,
+    pub status_media_low: bool,
+    pub status_media_needed: bool,
+    pub status_moving_to_paused: bool,
+    pub status_opc_life_over: bool,
+    pub status_opc_near_eol: bool,
+    pub status_output_area_almost_full: bool,
+    pub status_output_area_full: bool,
+    pub status_output_tray_missing: bool,
+    pub status_paused: bool,
+    pub status_shutdown: bool,
+    pub status_spool_area_full: bool,
+    pub status_stopped_partly: bool,
+    pub status_stopping: bool,
+    pub status_timed_out: bool,
+    pub status_toner_empty: bool,
+    pub status_toner_low: bool,
+    pub status_connection_to_device: bool,
+    pub status_offline_report: bool,
+    pub status_insecure_filter_warning: bool,
+    pub status_missing_filter_warning: bool,
+    pub status_remote_aborted: bool,
+    pub status_remote_canceled: bool,
+    pub status_remote_completed: bool,
+    pub status_remote_pending: bool,
+    pub status_remote_pending_held: bool,
+    pub status_remote_processing: bool,
+    pub status_remote_stopped: bool,
+    pub status_waiting_for_job_completed: bool,
+}
+
 pub fn printer_attr(pr_name: &str) {
     unsafe {
-        let mut cups_dest: *mut cups_dest_t = ptr::null_mut::<cups_dest_t>();
-        let pcups_dest = (&mut cups_dest) as *mut *mut cups_dest_t;
+        let cups_dest = cupsGetNamedDest(ptr::null::<http_t>(), pr_name.as_ptr(), ptr::null());
+        if !cups_dest.is_null() {
+            let cups_dest = &*cups_dest;
 
-        let n_dests = cupsGetDests(pcups_dest);
+            let result = Info::default();
 
-        for i in 0isize..n_dests as isize {
-            let cur_dest = &mut *cups_dest.offset(i);
-
-            dbg!("21");
-
-            let c_name = CStr::from_ptr(cur_dest.name);
-            let c_instance = if !cur_dest.instance.is_null() {
-                Some(CStr::from_ptr(cur_dest.instance))
+            result.printer_name = CStr::from_ptr(cups_dest.name).to_string_lossy().to_string();
+            result.printer_instance = if !cups_dest.instance.is_null() {
+                Some(
+                    CStr::from_ptr(cups_dest.instance)
+                        .to_string_lossy()
+                        .to_string(),
+                )
             } else {
                 None
             };
-            println!("{:?} {:?}", c_name, c_instance);
+
+            let opt = cupsGetOption(
+                "printer-uri-supported",
+                cups_dest.num_options,
+                cups_dest.options,
+            );
 
             let c_options = &*slice_from_raw_parts(cur_dest.options, cur_dest.num_options as usize);
+
+            // const char *cupsGetOption(const char *name, int num_options, cups_option_t *options);
+
             for opt in c_options {
                 let c_opt_name = CStr::from_ptr(opt.name);
                 let c_opt_value = CStr::from_ptr(opt.value);
                 println!("{:?}={:?}", c_opt_name, c_opt_value);
             }
+
+            // Starting with CUPS 1.2, the returned list of destinations
+            // include the "printer-info", "printer-is-accepting-jobs",
+            // "printer-is-shared", "printer-make-and-model", "printer-state",
+            // "printer-state-change-time", "printer-state-reasons",
+            // "printer-type", and "printer-uri-supported" attributes as options.
+            //
+            //     CUPS 1.4 adds the "marker-change-time", "marker-colors",
+            // "marker-high-levels", "marker-levels", "marker-low-levels",
+            // "marker-message", "marker-names", "marker-types", and
+            // "printer-commands" attributes as options.
+            //
+            //     CUPS 2.2 adds accessible IPP printers to the list of
+            // destinations that can be used. The "printer-uri-supported"
+            // option will be present for those IPP printers that have been
+            // recently used.
 
             let dinfo = cupsCopyDestInfo(ptr::null_mut::<http_t>(), cur_dest as *mut cups_dest_t);
 
@@ -100,7 +263,7 @@ pub fn printer_attr(pr_name: &str) {
                 CUPS_COPIES.as_ptr() as *const i8,
             );
             let copies = ippGetInteger(copies, 0);
-            println!("copies={}",copies);
+            println!("copies={}", copies);
 
             cupsFreeDestInfo(dinfo);
         }
